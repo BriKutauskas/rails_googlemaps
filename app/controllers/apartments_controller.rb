783f39b5370f989f32bf03fdfae3ad98fcdfc195
sharmila <sharmilahoskins@gmail.com>
2017-07-24 16:30:25 -0700

    added google map but not working

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -60,6 +60,7 @@ class ApartmentsController < ApplicationController
       format.json { head :no_content }
     end
   end
+  
 
   private
     # Use callbacks to share common setup or constraints between actions.
